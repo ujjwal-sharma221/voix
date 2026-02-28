35fed0b6c3255e9114b73a3f067cd2174cf42273
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2026-02-28 19:26:01 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { GoeyToaster } from "@/components/ui/goey-toaster";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoeyToaster spring={false} />
       </body>
     </html>
   );
